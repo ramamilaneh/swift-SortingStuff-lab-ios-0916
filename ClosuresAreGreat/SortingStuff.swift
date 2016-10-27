2d3915de8e4f6494549399f76930d131479e37a7
Rama Milaneh <ramamilaneh@Ramas-MacBook-Air.local>
2016-10-27 18:08:33 -0400

    Done

--- ClosuresAreGreat/SortingStuff.swift
+++ ClosuresAreGreat/SortingStuff.swift
@@ -15,10 +15,51 @@ struct ToyBin {
     var books: [Book] = []
     var bowlingPins: [BowlingPin] = []
     var musicCDs: [MusicCD] = []
+    mutating func sortShips() {
+        self.ships = self.ships.sorted { (first, second) -> Bool in
+            var result = false
+            if first.name == "Titanic" {
+                result = true
+            }
+            if first.age>second.age {
+                result = true
+            }
+            return result
+        }
+    }
     
-    // TODO: Implement all of the sort functions (lets organize this toy bin!)
+    mutating func sortBooks() {
+        self.books = self.books.sorted{ $0.name < $1.name}
+    }
     
- 
+    mutating func sortBowlingPins() {
+        self.bowlingPins = self.bowlingPins.sorted { first , second  in
+            return first.color.rawValue < second.color.rawValue
+        }
+    }
+    
+    mutating func sortMusicCDs() {
+        self.musicCDs = self.musicCDs.sorted(by: { (first, second) -> Bool in
+            var result = false
+            if first.name == "Dark" {
+                result = true
+            }
+            if first.name < second.name {
+                result = true
+            }
+           return result
+        })
+    }
+    
+    mutating func changeColorOfAllPins(to color: Color) {
+        
+        self.bowlingPins = self.bowlingPins.map{bowlingPin in
+            var newBowlingPin = bowlingPin
+            newBowlingPin.changeColor(to: color)
+            return newBowlingPin
+        }
+        
+    }
 }
 
 
@@ -39,6 +80,11 @@ struct BowlingPin {
     var name: String
     var film: String
     var color: Color
+    
+    
+    mutating func changeColor(to color: Color){
+       self.color = color
+    }
 }
 
 
--- ClosuresAreGreat/ViewController.swift
+++ ClosuresAreGreat/ViewController.swift
@@ -12,10 +12,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        performOperation(with: 5, 2, -9, operation: -)
+        
+        
+       
+        
+        }
+    
+    func performOperation(with numbers:Double... ,operation: @escaping (Double,Double) ->Double) {
+        var newNumbers = numbers
+         newNumbers.removeFirst()
+        let result = newNumbers.reduce(numbers[0]) { (total: Double, nextInLine: Double) -> Double in
+            print("Total is \(total)")
+            print("Next In Line is \(nextInLine)")
+            let resultOfOperation = operation(total, nextInLine)
+            print("Result of oepration is \(resultOfOperation)")
+            return resultOfOperation
+        }
+        
+        
+        print(result)
+    }
+    
     }
     
   
-    
 
     
-}
+
